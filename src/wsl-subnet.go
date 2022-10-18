64443bc593a9ffefcf2c9a2371a4af8a1f4d3173
Tobias Gruetzmacher <tobias.gruetzmacher@inform-software.com>
2022-10-18 11:22:42 +0200

    Calculate host address automatically from network

--- src/wsl-subnet.go
+++ src/wsl-subnet.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/Microsoft/hcsshim/hcn"
 	log "github.com/sirupsen/logrus"
+	"net/netip"
 )
 
 func ExistsWsl() bool {
@@ -36,8 +37,9 @@ func DeleteWsl() {
 }
 
 // This appears to work correctly.
-func CreateWsl(ipHost string, ipSubnet string) {
+func CreateWsl(prefix netip.Prefix) {
 	log.Info("Creating new WSL network...")
+	ipHost := prefix.Addr().Next()
 
 	newNet := hcn.HostComputeNetwork{
 		Name: "WSL",
@@ -45,17 +47,17 @@ func CreateWsl(ipHost string, ipSubnet string) {
 		Id:   "B95D0C5E-57D4-412B-B571-18A81A16E005",
 		Dns: hcn.Dns{
 			ServerList: []string{
-				ipHost,
+				ipHost.String(),
 			},
 		},
 		Ipams: []hcn.Ipam{
 			{
 				Subnets: []hcn.Subnet{
 					{
-						IpAddressPrefix: ipSubnet,
+						IpAddressPrefix: prefix.String(),
 						Routes: []hcn.Route{
 							{
-								NextHop:           ipHost,
+								NextHop:           ipHost.String(),
 								DestinationPrefix: "0.0.0.0/0",
 							},
 						},
@@ -72,8 +74,8 @@ func CreateWsl(ipHost string, ipSubnet string) {
 		log.Fatal(err)
 	}
 	log.WithFields(log.Fields{
-		"ip":   ipHost,
-		"net":  ipSubnet,
+		"ip":   ipHost.String(),
+		"net":  prefix.String(),
 		"guid": resultNet.Id,
 	}).Info("New WSL network created successfully")
 }
@@ -97,9 +99,13 @@ func main() {
 	var fDelete = flag.Bool("allow-delete", true, "allow deletion if existing WSL network")
 	var fCreate = flag.Bool("allow-create", true, "allow creation of network after deletion")
 	var fList = flag.Bool("list", false, "list all wsl networks before and after changes")
-	var fIpHost = flag.String("ip", "192.168.100.1", "WSL host machine IP address")
 	var fIpSubnet = flag.String("ipnet", "192.168.100.0/24", "WSL host subnet; must contain host address above")
 	flag.Parse()
+
+	prefix, err := netip.ParsePrefix(*fIpSubnet)
+	if err != nil || !prefix.Addr().Is4() {
+		log.Fatal("You didn't specify a valid IPv4 prefix")
+	}
 
 	if *fList {
 		log.Info("Networks before action:")
@@ -117,7 +123,7 @@ func main() {
 	}
 
 	if *fCreate {
-		CreateWsl(*fIpHost, *fIpSubnet)
+		CreateWsl(prefix)
 	}
 
 	if *fList {
